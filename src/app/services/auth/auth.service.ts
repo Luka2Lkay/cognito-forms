ff327bda4c2060351e56c96f52a62ea6f97e6333
Luka <lukhanyo95@gmail.com>
2024-10-22 18:04:31 +0200

    write signup service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserAttribute,
+  CognitoUserPool,
+} from "amazon-cognito-identity-js";
+import { environment } from "../../../environments/environment";
+import { User } from "../../interfaces/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  private userPool: CognitoUserPool;
+  private user: CognitoUser | null = null;
+  private isAuthenticated = new BehaviorSubject<boolean>(false);
+  public isAthenticated$ = this.isAuthenticated.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: environment.cognito.userPoolId,
+      ClientId: environment.cognito.userPoolWebClientId,
+    });
+  }
+
+  logIn() {}
+
+  register(data: User): Observable<void> {
+    const attributes = [
+      new CognitoUserAttribute({ Name: "email", Value: data.email }),
+    ];
+
+    return new Observable((observer) => {
+      this.userPool.signUp(
+        data.username,
+        data.password,
+        attributes,
+        [],
+        (err, result) => {
+          if (err) {
+            observer.error(err);
+          } else {
+            observer.next();
+            observer.complete();
+          }
+        }
+      );
+    });
+  }
 }
