5f5ab0581c592f254b1274f2934ff74bb1ee5847
Luka <lukhanyo95@gmail.com>
2024-10-23 12:30:43 +0200

    update json

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -8,12 +8,14 @@ import {
 } from "amazon-cognito-identity-js";
 import { environment } from "../../../environments/environment";
 import { User } from "../../interfaces/user";
+import {CognitoIdentityClient} from "@aws-sdk/client-cognito-identity"
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   private userPool: CognitoUserPool;
+  private cognitoClient: CognitoIdentityClient;
   private user: CognitoUser | null = null;
   private isAuthenticated = new BehaviorSubject<boolean>(false);
   public isAthenticated$ = this.isAuthenticated.asObservable();
@@ -23,6 +25,10 @@ export class AuthService {
       UserPoolId: environment.cognito.userPoolId,
       ClientId: environment.cognito.userPoolWebClientId,
     });
+
+    this.cognitoClient = new CognitoIdentityClient({
+
+    })
   }
 
   logIn() {}
@@ -33,20 +39,20 @@ export class AuthService {
     ];
 
     return new Observable((observer) => {
-      this.userPool.signUp(
-        data.userName,
-        data.password,
-        attributes,
-        [],
-        (err, result) => {
-          if (err) {
-            observer.error(err.message);
-          } else {
-            observer.next();
-            observer.complete();
-          }
-        }
-      );
+      // this.userPool.signUp(
+      //   data.userName,
+      //   data.password,
+      //   attributes,
+      //   [],
+      //   (err, result) => {
+      //     if (err) {
+      //       observer.error(err.message);
+      //     } else {
+      //       observer.next();
+      //       observer.complete();
+      //     }
+      //   }
+      // );
     });
   }
 }
