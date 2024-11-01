e11f810974a47479a4e5fbe36d87c20a1ca7609b
Luka <lukhanyo95@gmail.com>
2024-11-01 18:33:05 +0200

    generate update-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { ConfirmationComponent } from "./components/confirmation/confirmation.component";
 import { ProfileComponent } from "./components/profile/profile.component";
+import { UpdatePasswordComponent } from "./components/update-password/update-password.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "/register", pathMatch: "full" },
-  {path: "profile", component: ProfileComponent},
+  { path: "profile", component: ProfileComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  {path: "confirm", component: ConfirmationComponent}
+  { path: "confirm", component: ConfirmationComponent },
+  { path: "update-password", component: UpdatePasswordComponent },
 ];
