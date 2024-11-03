60971d8a40cc38bb0244091a33bb036928b28a21
Luka <lukhanyo95@gmail.com>
2024-11-03 17:44:34 +0200

    use routermodule

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { AuthService } from "../../services/auth/auth.service";
-import { Router } from "@angular/router";
+import { Router, RouterModule  } from "@angular/router";
 @Component({
   selector: "app-nav",
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: "./nav.component.html",
   styleUrl: "./nav.component.css",
 })
