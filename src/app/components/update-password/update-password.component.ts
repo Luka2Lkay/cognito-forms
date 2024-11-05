f1e7f29a1e46d5bb6a5df357c0861743c7a2fcfb
Luka <lukhanyo95@gmail.com>
2024-11-05 13:47:22 +0200

    change password inputs

--- src/app/components/update-password/update-password.component.ts
+++ src/app/components/update-password/update-password.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-update-password',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, ReactiveFormsModule],
   templateUrl: './update-password.component.html',
   styleUrl: './update-password.component.css'
 })
-export class UpdatePasswordComponent {
+export class UpdatePasswordComponent implements OnInit {
 
+  constructor(private _fb: FormBuilder){}
+
+  ngOnInit(): void {
+    
+  }
+
+  changePasswordForm: FormGroup = this._fb.group({
+    oldPassword: "",
+    newPassword: ""
+  })
+
+  changePassword(){
+    console.log(this.changePasswordForm.value)
+  }
 }
